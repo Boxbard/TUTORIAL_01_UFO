9b2f4a0509eeaa3ca26acc40ad438c4cce268b21
Boxbard <54591758+Boxbard@users.noreply.github.com>
2019-09-22 23:56:28 -0400

    UFO_Challenge

--- CASEY_CHUNG_TUTORIAL_01_UFO/Assets/Scripts/PlayerController.cs
+++ CASEY_CHUNG_TUTORIAL_01_UFO/Assets/Scripts/PlayerController.cs
@@ -7,19 +7,25 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public Text countText;
+    public Text livesText;
     public Text winText;
+    
 
     private Rigidbody2D rb2d;
     private int count;
+    private int lives;
 
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         count = 0;
+        lives = 3;
         winText.text = "";
         SetCountText();
+        SetLivesText();
     }
-    void FixedUpdate()
+
+    private void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
@@ -38,13 +44,30 @@ public class PlayerController : MonoBehaviour
             count = count + 1;
             SetCountText();
         }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetLivesText();
+        }
+        if (count == 12)
+        {
+            transform.position = new Vector2(75.0f, 00.0f);
+        }
     }
-    void SetCountText()
+
+    private void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if (count >= 12)
+        
+        if (count >= 20)
         {
             winText.text = "You win! Game created by Casey Chung";
         }
+    }
+    private void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+
     }
 }
